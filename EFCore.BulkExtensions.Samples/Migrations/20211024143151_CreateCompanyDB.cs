fcace70f740d3d92dfd2c0601f7a91849f6da43d
Gopichandar <gopichandarbe@gmail.com>
2021-10-24 20:34:40 +0530

    Initial code

---
+++ EFCore.BulkExtensions.Samples/Migrations/20211024143151_CreateCompanyDB.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.BulkExtensions.Samples.Migrations
+{
+    public partial class CreateCompanyDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
--- EFCore.BulkExtensions.Samples/Program.cs
+++ EFCore.BulkExtensions.Samples/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 
 namespace EFCore.BulkExtensions.Samples
 {
@@ -6,7 +8,62 @@ namespace EFCore.BulkExtensions.Samples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var context = new CompanyContext())
+            {
+                context.Database.Migrate();
+                var cust = new Customer()
+                {
+                    Name = "Kayle"
+                };
+
+                context.Customers.Add(cust);
+                context.SaveChanges();
+            }
+
+
+            var customers = new List<Customer>();
+            customers.Add(new Customer() { Name = "John" });
+            customers.Add(new Customer() { Name = "Smith" });
+            customers.Add(new Customer() { Name = "Kayle" });
+
+
+            using (var context = new CompanyContext())
+            {
+                context.BulkInsertOrUpdate(customers, b =>
+                {
+                    b.UpdateByProperties = new List<string> { nameof(Customer.Name) };
+                });
+            }
+
+            //I'm expecting the following output as a response in customer;  How to achieve this? Any Idea
+
+            Console.WriteLine($"{customers[0].Id}, {customers[0].Name}");    //   2, John
+            Console.WriteLine($"{customers[1].Id}, {customers[1].Name}");    //   3, Smith
+            Console.WriteLine($"{customers[2].Id}, {customers[2].Name}");    //   1, Kayle
         }
+    }
+
+    public class CompanyContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=sample.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Customer>()
+                .HasIndex(u => u.Name)
+                .IsUnique();
+        }
+    }
+
+    //Model
+    public class Customer
+    {
+        public int Id { get; set; }        
+        public string Name { get; set; }
     }
 }
